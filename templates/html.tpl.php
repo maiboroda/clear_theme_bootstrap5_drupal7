0905beee2390d554a62f803906a88d1f1f5dc130
Mayboroda Vladimir <maiboroda-vladimir@yandex.ru>
2018-10-29 11:08:17 +0300

    Вывод региона по умолчанию, хлебные крошки, загловок

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title><?php print $head_title; ?></title>
-    <?php print $style; ?>
+    <?php print $styles; ?>
     <?php print $scripts; ?>
   </head>
   <body>
--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -23,7 +23,15 @@
     <div class="container-fluid">
         <div class="container">
             <div class="row">
-                <div class="col-md-12 col-lg-12 col-sm-12"></div>
+                <div class="col-md-12 col-lg-12 col-sm-12">
+                    <nav aria-label="breadcrumb">
+                        <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
+                    </nav>
+                    <?php if (!empty($title)): ?>
+                        <h1><?php print $title; ?></h1>
+                    <?php endif; ?>
+                    <?php print render($page['content']); ?>
+                </div>
             </div>
         </div>
     </div>
